cf3c37273272cf8e7619a813633287d100e14d8a
Nadav Shalev <nadav.shalev@fyber.com>
2019-09-10 13:38:14 +0300

    Added fairbid sdk version

--- Assets/Scenes/MainScene.cs
+++ Assets/Scenes/MainScene.cs
@@ -17,6 +17,7 @@ using UnityEngine;
 using Fyber;
 using System;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// The Main Scene,
@@ -30,11 +31,13 @@ public class MainScene : MonoBehaviour {
     /// </summary>
     /// TODO replace with your own app id.
     private const String PUBLISHER_APP_ID = "109613";
+    
 
     /// <summary>
     /// Start this instance.
     /// </summary>
     void Start() {
+        setFaBidVersionTextView();
         startFairBidSdk(PUBLISHER_APP_ID);
     }
 
@@ -59,5 +62,12 @@ public class MainScene : MonoBehaviour {
     /// </summary>
     public void ShowTestSuite() {
         FairBid.ShowTestSuite();
+    }
+
+
+    private void setFairBidVersionTextView()
+    {
+        Text fairbidTextViewVersion = transform.Find("Background/FyberVersionTV").GetComponent<Text>();
+        fairbidTextViewVersion.text = "FYBER FAIRBID " + FairBid.Version;
     }
 }
